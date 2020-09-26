3390518175a54646028927f3c5d340972299c300
kmeawad <71085124+kmeawad@users.noreply.github.com>
2020-09-26 19:00:03 -0400

    Add files via upload

---
+++ DoubleLinkedList.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kareem
+ */
+public class DoubleLinkedList <A> {
+
+    
+    private static class Node<A>{
+        private A Input;
+        private Node<A> nextNode;
+        private Node<A> prevNode;
+
+        public Node(A Input, Node<A> nextNode, Node<A> prevNode) {
+            this.Input = Input;
+            this.nextNode = nextNode;
+            this.prevNode = prevNode;
+        }
+        public A getInput(){
+            return Input;
+        }
+        
+        public void setNext(Node<A> nextNode){
+            this.nextNode = nextNode;
+        }
+        public Node<A> getNext(){
+            return nextNode;
+        }
+        
+        public void setprev(Node<A> prevNode){
+            this.prevNode = prevNode;
+        }
+        public Node<A> getprev(){
+            return prevNode;
+        }
+    }
+    
+    int size = 0;
+    private Node<A> head;
+    private Node<A> tail;
+    
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    public void addFirst(A element){
+        Node<A> newNode = new Node<>(element, head , null); // 
+        
+        if(isEmpty()) tail = newNode;
+        if(size != 0) head.setprev(newNode); 
+        head = newNode;
+        size++;
+    }
+    
+    public void addLast(A element){
+        Node<A> newNode = new Node<>(element, null, tail);
+        
+        if(isEmpty()) head = newNode;
+        else {
+            tail.setNext(newNode);
+        }
+
+        tail = newNode;
+        size++;
+    }
+    
+    public void printAll()
+    {
+        Node<A> currentNode = head;
+        while(currentNode != null)
+        {
+            System.out.println(currentNode.getInput());
+            currentNode = currentNode.getNext();
+        }
+    }
+    public void printReverse(){
+        Node<A> currentNode = tail;
+        while(currentNode != null)
+        {
+            System.out.println(currentNode.getInput());
+            currentNode = currentNode.getprev();
+        }
+    }
+}
