cade875283ed37fda6cd4f99405e7074a301d776
kmeawad <71085124+kmeawad@users.noreply.github.com>
2020-09-29 17:29:47 -0400

    Add files via upload

---
+++ Rectangle.js
@@ -0,0 +1,36 @@
+
+function shape(type, height, width, displacement){
+    let triangle = "";
+    type(height, width, displacement);
+}
+
+function rectangle(height , width, displacement){
+        let rectangle = "";
+        for(let i = 0; i < height; i++){
+            
+            //Add line
+            line ="\n";
+
+            //Create an offset
+            let offset = "";
+            for(let o =0; o<=displacement;o++) offset += " ";
+
+            line+=offset;
+
+            //Add stars 
+            for(let w=0;w<width;w++)
+                line += "*";
+            
+            //Add each line to rectangle
+            rectangle +=line;
+        }
+        console.log(rectangle);
+}
+//if (type == 1){
+
+//}
+
+shape(rectangle, 5, 5, 10);
+//shape(6, 6, 1, 1, 10);
+//shape(2, 2, 1, 1, 20);
+//shape(3, 3, 1, 1, 20);
